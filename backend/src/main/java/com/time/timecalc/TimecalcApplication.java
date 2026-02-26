bcf1218b88d8a1576450ccc4c98c60c37c764aa9
Даниил Путинцев <daniilputincev91@gmail.com>
2026-02-26 05:14:04 +0400

    Проект

---
+++ backend/src/main/java/com/time/timecalc/TimecalcApplication.java
@@ -0,0 +1,13 @@
+package com.time.timecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimecalcApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/time/timecalc/TimecalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.timecalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimecalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
